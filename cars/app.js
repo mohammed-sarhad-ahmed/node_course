46e205d6fe064b6694ca74e15912e2a34e6cccad
mohammed-sarhad-ahmed <120845827+mohammed-sarhad-ahmed@users.noreply.github.com>
2025-04-26 16:57:59 +0300

    Update app.js

--- cars/app.js
+++ cars/app.js
@@ -2,21 +2,19 @@ import express from "express";
 import carsRouter from "./controller/cars.js";
 import mongoose from "mongoose";
 import morgan from "morgan";
-import config from "dotenv/config";
+import dotenv from "dotenv";
 
 if (process.env.NODE_ENV === "dev") {
-  config({
+  dotenv.config({
     path: "./dev.env",
   });
 } else {
-  config({
+  dotenv.config({
     path: "./prod.env",
   });
 }
 
-console.log(process.env.PORT);
-
-await mongoose.connect("mongodb://localhost:27017/Cars");
+await mongoose.connect(process.env.DATABASE);
 
 const app = express();
 app.use(express.json());
@@ -24,6 +22,6 @@ app.use(morgan("short"));
 
 app.use("/cars", carsRouter);
 
-app.listen(5050, () => {
-  console.log("started a server at port 5050");
+app.listen(process.env.PORT, () => {
+  console.log(`started a server at port ${process.env.PORT} `);
 });
